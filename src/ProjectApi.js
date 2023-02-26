a18f3f6ef29ed9abfd0f4f6af4300faa0d00b6c2
ThedaLicious <ThedaLicious@users.noreply.github.com>
2023-02-26 01:25:56 -0500

    Update ProjectApi.js

--- src/ProjectApi.js
+++ src/ProjectApi.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
+import axios from 'axios';  //import dependency called axios- which lets fe talk to be
 
 export default axios.create({
-    baseURL: 'https://infinite-journey-94180.herokuapp.com/api'
+    baseURL: 'https://infinite-journey-94180.herokuapp.com/api' // bkend adDy
 });
